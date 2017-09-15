9cffd248df1154802e6c844e2cd18fc4c8a8339b
trishnendu <trishnendu@outlook.com>
2017-09-15 15:42:09 +0530

    auto parse table creation complete

--- 4th_assignment/parser_auto.cpp
+++ 4th_assignment/parser_auto.cpp
@@ -67,76 +67,101 @@ class Grammer{
             return true;
         }
 
-        set<string> create_followset(string symb){
-            if(FOLLOW.find(symb) == FOLLOW.end())
-                FOLLOW.insert(make_pair(symb, set<string>()));
-            for(vector <pair <string, vector <string>>>::iterator iit = rules.begin(); iit != rules.end(); iit++){
-                if(iit->first != symb){
-                    vector<string>::iterator tmp = find(iit->second.begin(), iit->second.end(), symb);
-                    if(tmp + 1 < iit->second.end()){
-                        FOLLOW[symb].insert(FIRST[*(tmp+1)].begin(), FIRST[*(tmp+1)].end());
-                        if(FIRST[*(tmp+1)].find(EPS) != FIRST[*(tmp+1)].end()){
-                            set<string> tmpset = create_followset(iit->first);
-                            FOLLOW[symb].insert(tmpset.begin(), tmpset.end());
-                        }
-                    } 
-                    if(tmp == iit->second.end()){
-                        set<string> tmpset = create_followset(iit->first);
-                        FOLLOW[symb].insert(tmpset.begin(), tmpset.end());
-                    }
-                }
-            }
-            FOLLOW[symb].erase(EPS);
-            return FOLLOW[symb];   
-        }
-
         bool create_followset(){
+            map <string, set<string>> EQU;  
             FOLLOW.insert(make_pair(S, set<string>{"$"}));
             for(set<string>::iterator it = v.begin(); it != v.end(); it++){
                 if(FOLLOW.find(*it) == FOLLOW.end())
                     FOLLOW.insert(make_pair(*it, set<string>()));
+                EQU.insert(make_pair(*it, set<string>()));
                 for(vector <pair <string, vector <string>>>::iterator iit = rules.begin(); iit != rules.end(); iit++){
                     if (*it != iit->first){
                     //cout << iit->second.front() << endl;
                         vector<string>::iterator tmp = find(iit->second.begin(), iit->second.end(), *it);
-                        if(tmp + 1 < iit->second.end()){
+                        if(tmp < iit->second.end() - 1){
                             FOLLOW[*it].insert(FIRST[*(tmp+1)].begin(),FIRST[*(tmp+1)].end());
-                            if(FIRST[*(tmp+1)].find(EPS) != FIRST[*(tmp+1)].end()){
-                                set<string> tmpset = create_followset(iit->first);
-                                FOLLOW[*it].insert(tmpset.begin(), tmpset.end());
-                            }
-                        } 
-                        if(tmp == iit->second.end()){
-                            set<string> tmpset = create_followset(iit->first);
-                            FOLLOW[*it].insert(tmpset.begin(), tmpset.end());
-                        }
+                            if(FIRST[*(tmp+1)].find(EPS) != FIRST[*(tmp+1)].end())
+                                EQU[*it].insert(iit->first);
+                        } else if(tmp == iit->second.end() - 1)
+                            EQU[*it].insert(iit->first);
                     }
                     FOLLOW[*it].erase(EPS);
+                }
+            }
+            for(set<string>::iterator it = v.begin(); it != v.end(); it++){
+                for(set<string>::iterator iit = v.begin(); iit != v.end(); iit++){
+                    for(set<string>::iterator ele = EQU[*iit].begin(); ele != EQU[*iit].end(); ele++){
+                        //cout << "updating " << *iit << " with " << *ele << endl;
+                        FOLLOW[*iit].insert(FOLLOW[*ele].begin(), FOLLOW[*ele].end());
+                    }
                 }
             }
             return true;
         }
 
         bool create_ptable(){
+            cout << "RULES" << endl;
+            for(vector <pair <string, vector <string>>>::iterator iit = rules.begin(); iit != rules.end(); iit++){
+                cout << iit->first << " -> ";
+                for(vector<string>::iterator str = iit->second.begin(); str != iit->second.end(); str++){
+                    cout << *str;
+                }
+                cout << endl;
+            }
             create_firstset();
-        /*    cout << "FIRST SET" << endl;
+            cout << "\nFIRST SET" << endl;
             for(map <string, set<string>>::iterator it = FIRST.begin(); it != FIRST.end(); it++){
                 cout << it->first << " -> ";
                 for(set<string>::iterator iit = it->second.begin(); iit != it->second.end(); iit++){
                     cout << *iit << ", ";
                 }
                 cout << endl;
-            }   */
+            }  
             create_followset();
-                cout << "FOLLOW SET" << endl;
+            cout << "\nFOLLOW SET" << endl;
             for(map <string, set<string>>::iterator it = FOLLOW.begin(); it != FOLLOW.end(); it++){
                 cout << it->first << " -> ";
                 for(set<string>::iterator iit = it->second.begin(); iit != it->second.end(); iit++){
                     cout << *iit << ", ";
                 }
                 cout << endl;
-            }   
-        
+            }  
+            for(vector <pair <string, vector <string>>>::iterator iit = rules.begin(); iit != rules.end(); iit++){
+                for(vector<string>::iterator rhs_symb =  iit->second.begin(); rhs_symb != iit->second.end(); iit++){
+                    bool eps_exists = false;
+                    if(*rhs_symb == EPS){
+                        for(set<string>::iterator b = FOLLOW[iit->first].begin(); b != FOLLOW[iit->first].end(); b++){
+                            parsing_table.insert(make_pair(make_pair(iit->first, *b), iit->second));    
+                        }
+                        break;
+                    } 
+                    for(set<string>::iterator a = FIRST[*rhs_symb].begin(); a != FIRST[*rhs_symb].end(); a++){
+                        if(*a == EPS){
+                            eps_exists = true;
+                            cout << "debugging ... found EPS @" << iit->first << endl; 
+                            for(set<string>::iterator b = FOLLOW[iit->first].begin(); b != FOLLOW[iit->first].end(); b++){
+                                parsing_table.insert(make_pair(make_pair(iit->first, *b), iit->second));    
+                            }
+                        }    
+                        else
+                            parsing_table.insert(make_pair(make_pair(iit->first, *a), iit->second));
+                    }
+                    if(!eps_exists)
+                        break;
+                }
+            }
+            cout << "\nPARSING TABLE" << endl;
+            for(set <string>::iterator row = v.begin(); row != v.end(); row++){
+                for(set<string>::iterator col = sigma.begin(); col != sigma.end(); col++){
+                    if(parsing_table.find(make_pair(*row, *col)) != parsing_table.end()){
+                        cout << *row << " AND " << *col << "| " << *row << " =>";
+                        vector<string> derivation = parsing_table[make_pair(*row, *col)];
+                        for(int i = 0; i < derivation.size(); i++)
+                            cout << derivation[i];
+                        cout << endl;
+                    }
+                }
+            }
             return true;
         }
 
@@ -150,7 +175,7 @@ class Grammer{
                 } else{
                     string head = *(pstack.end()-1);
                     pstack.pop_back();
-                    cout << "stack-head: "  << head << ", "<< "current symb: " << *buffer_index << endl;
+                    //cout << "stack-head: "  << head << ", "<< "current symb: " << *buffer_index << endl;
                     if (*buffer_index == head){
                         buffer_index++;
                     } else if(parsing_table.find(make_pair(head,*buffer_index)) != parsing_table.end()){
@@ -168,7 +193,7 @@ class Grammer{
         }
 };
 
-int main(){
+/*int main(){
     Grammer G(set<string>{"E","E'","T","T'","F"}, set<string>{"id","+","*","(","$",")"}, "E");    
     G.insert_rule("E", vector<string>{"T","E'"});
     G.insert_rule("E'", vector<string>{"+","T","E'"});
@@ -181,29 +206,16 @@ int main(){
     if(G.create_ptable()){
         cout << ":)" << endl;
     }
-/*    G.insert_ptable("E", "id", vector<string>{"T","E'"});
-    G.insert_ptable("T", "id", vector<string>{"F","T'"});
-    G.insert_ptable("F", "id", vector<string>{"id"});
-    G.insert_ptable("E'", "+", vector<string>{"+","T","E'"});
-    G.insert_ptable("T'", "+", vector<string>{EPS});
-    G.insert_ptable("T'", "*", vector<string>{"*","F","T'"});
-    G.insert_ptable("E", "(", vector<string>{"T","E'"});
-    G.insert_ptable("T", "(", vector<string>{"F","T'"});
-    G.insert_ptable("F", "(", vector<string>{"(","E",")"});
-    G.insert_ptable("E'", ")", vector<string>{EPS});
-    G.insert_ptable("T'", ")", vector<string>{EPS});
-    G.insert_ptable("E'", "$", vector<string>{EPS});
-    G.insert_ptable("T'", "$", vector<string>{EPS});*/
-/*    if(G.parse_string(vector<string>{"id","+","id","*","id"})){
+    if(G.parse_string(vector<string>{"id","+","id","*","id"})){
         cout << "Parsing successful! Correct syntax :)" << endl;
     } else{
         cout << "Parsing faild! Incorrect syntax :(" << endl;
-    }*/
+    }
     return 0;
     
-}
-/*
-int main1(){
+}*/
+
+int main(){
     Grammer G(set<string>{"S","A","B"}, set<string>{"a","b","c","$"}, "S");    
     G.insert_rule("S", vector<string>{"a","A","a"});
     G.insert_rule("S", vector<string>{"B","A","a"});
@@ -212,17 +224,20 @@ int main1(){
     G.insert_rule("A", vector<string>{"b","A"});
     G.insert_rule("A", vector<string>{EPS});
     G.insert_rule("B", vector<string>{"b"});
-    G.insert_ptable("S", "a", vector<string>{"a","A","a"}); 
+    if(G.create_ptable()){
+        cout << ":)" << endl;
+    }
+/*    G.insert_ptable("S", "a", vector<string>{"a","A","a"}); 
     G.insert_ptable("A", "a", vector<string>{EPS}); 
     G.insert_ptable("S", "b", vector<string>{"B","A","a"}); 
     G.insert_ptable("A", "b", vector<string>{"b","A"}); 
     G.insert_ptable("B", "b", vector<string>{"b"}); 
     G.insert_ptable("A", "c", vector<string>{"c","A"}); 
-    G.insert_ptable("S", "$", vector<string>{EPS}); 
+    G.insert_ptable("S", "$", vector<string>{EPS}); */
     if(G.parse_string(vector<string>{"b","c","b","a"})){
         cout << "Parsing successful! Correct syntax :)" << endl;
     } else{
         cout << "Parsing faild! Incorrect syntax :(" << endl;
     }
     return 0;
-}*/
+}
